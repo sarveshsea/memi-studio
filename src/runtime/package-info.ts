4557833579916f38858fe2d62d327a1421e35755
sarveshsea <sarveshjax@gmail.com>
2026-06-07 11:47:05 -0500

    chore(studio): prepare v1.0.4 release

--- src/runtime/package-info.ts
+++ src/runtime/package-info.ts
@@ -4,4 +4,4 @@
 export const MEMOIRE_PACKAGE_NAME = "@memi-design/cli";
 export const MEMOIRE_PACKAGE_VERSION = "1.1.0";
 export const MEMOIRE_PACKAGE_URL = "https://www.npmjs.com/package/@memi-design/cli";
-export const MEMOIRE_STUDIO_VERSION = "1.0.3";
+export const MEMOIRE_STUDIO_VERSION = "1.0.4";
